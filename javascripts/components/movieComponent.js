3d032334efdc0b55e423ee9b340d8c27c1b4f830
Maggie Leavell <mjleavell@gmail.com>
2018-10-24 18:56:02 -0500

    added more data, updated search function

--- javascripts/components/movieComponent.js
+++ javascripts/components/movieComponent.js
@@ -1,12 +1,12 @@
 const printMovie = (movieArray) => {
     let domString = '';
     movieArray.forEach((movie) => {
-        domString += `<div class="card movie d-block mx-auto bg-light" style="width: 25rem;">`
+        domString += `<div class="card movie d-flex m-2 bg-light" style="width: 25rem;">`
         domString += `<div class="card-body">`
         domString += `<h5 class="card-title text-center">${movie.name}</h5>`
         domString += `<h6 class="card-subtitle mb-2 text-center">Genre: ${movie.genre}</h6>`
         domString += `<h6 class="card-subtitle mb-2 text-center">Release Date: ${movie.releaseDate}</h6>`
-        domString += `<p class="card-text text-center">${movie.description}</p>`
+        domString += `<p class="card-text text-justify">${movie.description}</p>`
         domString += `</div>`
         domString += `</div>`
     })
